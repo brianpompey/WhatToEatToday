7c8d52cc585e569f701bc3433168a16d54b8328f
brianpompey <brianthescholar@gmail.com>
2020-09-20 12:37:56 -0400

    not sure where to put frontend files but thisll work

---
+++ application.js
@@ -0,0 +1,57 @@
+console.log("Hello")
+
+fetch('http://localhost:3001/restaurants')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+/*
+const BASE_URL = "http://localhost:3001";
+const RESTAURANTS_URL = `${BASE_URL}/restaurants`;
+
+const container = document.getElementById("container");
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderRestaurants();
+});
+
+const renderRestaurants = async () => {
+  try {
+    const restaurants = await getRestaurants();
+    for (const restaurant of restaurants) {
+        container.innerHTML = ("[" + restaurants[i].id + "]: " + restaurants[i].name) + "<br>"
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+const getRestaurants = async () => {
+    try {
+      const response = await fetch(`${RESTAURANTS_URL}`);
+      const json = await response.json();
+      return json.data;
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+
+function getRestaurants() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let container = document.getElementById("container");
+            let responses = JSON.parse(this.responseText)
+
+            for (var i = 0; i < responses.length; i++) {
+                container.insertAdjacentHTML("beforeend", "[" + responses[i].id + "]: " + responses[i].name) + "<br>"
+            };
+        }
+    };
+    xhttp.open("GET", "http://localhost:3001/restaurants", true)
+    xhttp.send();
+}
+
+renderRestaurants();
+*/
